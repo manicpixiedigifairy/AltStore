05fad2753c94574eac4bc10349ec9abe49a4ce0d
Riley Testut <riley@rileytestut.com>
2024-08-28 14:50:02 -0500

    Updates TelemetryDeck App ID for non-MARKETPLACE builds

--- AltStore/Analytics/AnalyticsManager.swift
+++ AltStore/Analytics/AnalyticsManager.swift
@@ -18,7 +18,7 @@ private let telemetryDeckAppID = "67F64B51-C3E4-42A5-9CA2-300CCAFA55C9"
 
 #else
 
-private let telemetryDeckAppID = ""
+private let telemetryDeckAppID = "D791762B-8577-4B25-B7EA-9241F6B07BF4"
 
 #endif
 
